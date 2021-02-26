b34048ed91e05420e973d0e84b6b7bf68b8385c2
deanjheikkila <deanjheikkila@gmail.com>
2021-02-25 20:01:18 -0800

    minor fixes

--- src/content/pages/recipes/NewRecipe.js
+++ src/content/pages/recipes/NewRecipe.js
@@ -60,54 +60,68 @@ export default function NewRecipe(props) {
 
 
   return (
-    <div className="form-background">
-      <h2 className="form-title">New Recipe</h2>
-      <span className="red">{message}</span>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Title:</label>
-          <input type="text" name="title" onChange={e => setTitle(e.target.value)} />
-        </div>
-        <div>
-          <label>Alt tag:</label>
-          <input type="text" name="alt" onChange={e => setAlt(e.target.value)} />
-        </div>
-        <div>
-          <label>Image url:</label>
-          <input type="url" name="image" onChange={e => setImage(e.target.value)} />
-          {!image ? "":
-            <div>
-              <p>Image Preview:</p>
-              <img src={image} alt={alt} />
-            </div>
-          }
-        </div>
-        <div>
-          <label>Servings:</label>
-          <input type="number" name="servings" onChange={e => setServings(e.target.value)} />
-        </div>
-        <div>
-          <label>Description: ({chars} character{chars===1?'':'s'} remaining)</label>
-          <input type="text" name="description" placeholder="Describe your recipe" onChange={(e) => {
-            setDescription(e.target.value);
-            setChars(280 - e.target.value.length);
-          }} />
-        </div>
-        <div>
-          <label>Directions:</label>
-          <input type="text" name="directions" placeholder="Comma-separate your list of directions..." onChange={e => setDirections(e.target.value)} />
-        </div>
-        <div>
-          <label>Ingredients:</label>
-          <input type="text" name="ingredients" placeholder="Comma-separate your list of ingredients..." onChange={e => setIngredients(e.target.value)} />
-        </div>
-        <div>
-          <label>Tags:</label>
-          <input type="text" name="tags" placeholder="Comma-separate your list of tags, the more the better!" onChange={e => setTags(e.target.value)} />
-        </div>
-        <button className="form-button" type="submit">Create Recipe!</button>
-      </form>
-      {redirect}
+    <div className="recipe-form">
+      <h2 className="author thumbnail-tab">{props.user.name}'s New Recipe</h2>
+      <div className="thumbnail-content">
+        <form onSubmit={handleSubmit}>
+          <span className="red">{message}</span>
+          <div>
+            <label>Title:</label>
+            <input type="text" name="title" 
+              placeholder="Something tasty"
+              onChange={e => setTitle(e.target.value)} 
+            />
+          </div>
+          <div>
+            <label>Image URL:</label>
+            <input type="url" name="image" onChange={e => setImage(e.target.value)} />
+            {!image ? "":
+              <div>
+                <p>Image Preview:</p>
+                <img src={image} alt={alt} />
+              </div>
+            }
+          </div>
+          <div>
+            <label>Alt tag:</label>
+            <input type="text" name="alt" 
+              placeholder="add an alt tag for your image"
+              onChange={e => setAlt(e.target.value)} 
+            />
+          </div>
+          <div>
+            <label>Number of Servings:</label>
+            <input type="number" name="servings" 
+              placeholder="0"
+              onChange={e => setServings(e.target.value)} 
+            />
+          </div>
+          <div>
+            <label>Description: ({chars} character{chars===1?'':'s'} remaining)</label>
+            <input type="text" name="description" 
+            placeholder="Describe your recipe" 
+              onChange={(e) => {
+                setDescription(e.target.value);
+                setChars(280 - e.target.value.length);
+              }} 
+            />
+          </div>
+          <div>
+            <label>Directions:</label>
+            <input type="text" name="directions" placeholder="Comma-separate your list of directions..." onChange={e => setDirections(e.target.value)} />
+          </div>
+          <div>
+            <label>Ingredients:</label>
+            <input type="text" name="ingredients" placeholder="Comma-separate your list of ingredients..." onChange={e => setIngredients(e.target.value)} />
+          </div>
+          <div>
+            <label>Tags:</label>
+            <input type="text" name="tags" placeholder="Comma-separate your list of tags, the more the better!" onChange={e => setTags(e.target.value)} />
+          </div>
+          <button className="form-button" type="submit">Create Recipe!</button>
+        </form>
+        {redirect}
+      </div>
     </div>
 
   )
