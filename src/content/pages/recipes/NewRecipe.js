f0a226baf1cd35d6f0dd831b3914238d61fc7d08
deanjheikkila <deanjheikkila@gmail.com>
2021-03-01 21:40:03 -0800

    added user friendly functionality to recipe form

--- src/content/pages/recipes/NewRecipe.js
+++ src/content/pages/recipes/NewRecipe.js
@@ -1,22 +1,27 @@
 // Packages
 import React, { useState, useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
-import axios from 'axios';
+// import axios from 'axios';
 
 export default function NewRecipe(props) {
   // Declare and initialize state variables
   let [message, setMessage] = useState("");
   let [redirect, setRedirect] = useState("");
   let [title, setTitle] = useState('');
-  let [alt, setAlt] = useState('');
   let [image, setImage] = useState('');
-  let [servings, setServings] = useState(0);
-
+  let [alt, setAlt] = useState('');
   let [description, setDescription] = useState("");
-  let [directions, setDirections] = useState("");
-  let [ingredients, setIngredients] = useState("");
-  let [tags, setTags] = useState("");
+  let [servings, setServings] = useState(0);
   let [chars, setChars] = useState(280);
+
+  let [newIngredient, setNewIngredient] = useState('');
+  let [ingredients, setIngredients] = useState([]);
+
+  let [newDirection, setNewDirection] = useState('');
+  let [directions, setDirections] = useState([]);
+
+  let [newTag, setNewTag] = useState('')
+  let [tags, setTags] = useState([]);
 
   useEffect(()=> {
 
@@ -35,9 +40,9 @@ export default function NewRecipe(props) {
         image,
         servings,
         description,
-        directions,
-        ingredients,
-        tags
+        directions: directions.join(),
+        ingredients: ingredients.join(),
+        tags: tags.join()
       }),
       headers: {
         'Content-Type': 'application/json',
@@ -58,17 +63,47 @@ export default function NewRecipe(props) {
     return <Redirect to="/auth/login" />
   }
 
+  function makeList(items, setItems) {
+    return (
+      <>
+        {items.map((item, i) => (
+          <li>
+          {item} 
+          <button 
+            onClick={() => {
+              // deletes the item
+              let tempItems = [...items]
+              tempItems.splice(i, 1)
+              setItems(tempItems)
+            }}
+          >
+            X
+          </button>
+        </li>
+        ))}
+      </>
+    )
+  }
+
+  function addItem(items, setItems, newItem, setNewItem) {
+    if (newItem) {
+      // first replace commas with fancy commas
+      // setNewItem(newItem.replace(/,/g, ';'))
+      setItems([...items, newItem]);
+    }
+    setNewItem('');
+  }
 
   return (
-    <div className="recipe-form">
+    <div className="recipe-form-container">
       <h2 className="author thumbnail-tab">{props.user.name}'s New Recipe</h2>
       <div className="thumbnail-content">
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={(e) => e.preventDefault()} className="recipe-form">
           <span className="red">{message}</span>
           <div>
             <label>Title:</label>
             <input type="text" name="title" 
-              placeholder="Something tasty"
+              placeholder="something tasty"
               onChange={e => setTitle(e.target.value)} 
             />
           </div>
@@ -78,7 +113,15 @@ export default function NewRecipe(props) {
             {!image ? "":
               <div>
                 <p>Image Preview:</p>
-                <img src={image} alt={alt} />
+                <div 
+                  style={{backgroundImage: 'url(' + image + ')'}}
+                  alt={alt} 
+                  className="recipe-img"
+                >
+                  <div className="recipe-img-text">
+                    <h3>{description}</h3>
+                  </div>
+                </div>
               </div>
             }
           </div>
@@ -87,13 +130,6 @@ export default function NewRecipe(props) {
             <input type="text" name="alt" 
               placeholder="add an alt tag for your image"
               onChange={e => setAlt(e.target.value)} 
-            />
-          </div>
-          <div>
-            <label>Number of Servings:</label>
-            <input type="number" name="servings" 
-              placeholder="0"
-              onChange={e => setServings(e.target.value)} 
             />
           </div>
           <div>
@@ -107,18 +143,62 @@ export default function NewRecipe(props) {
             />
           </div>
           <div>
-            <label>Directions:</label>
-            <input type="text" name="directions" placeholder="Comma-separate your list of directions..." onChange={e => setDirections(e.target.value)} />
+            <label>Number of Servings:</label>
+            <input type="number" name="servings" 
+              placeholder="0"
+              onChange={e => setServings(e.target.value)} 
+            />
           </div>
           <div>
             <label>Ingredients:</label>
-            <input type="text" name="ingredients" placeholder="Comma-separate your list of ingredients..." onChange={e => setIngredients(e.target.value)} />
+            <ul>
+              {makeList(ingredients, setIngredients)}
+            </ul>
+            <input 
+              type="text"
+              placeholder="Comma-separate your list of ingredients..." 
+              onChange={e => setNewIngredient(e.target.value)} 
+            />
+            <button
+              onClick={() => addItem(ingredients, setIngredients, newIngredient, setNewIngredient)}
+            >
+              Add
+            </button>
+          </div>
+          <div>
+            <label>Directions:</label>
+            <ol>
+              {makeList(directions, setDirections)}
+            </ol>
+            <input 
+              type="text" 
+              placeholder="Preheat oven to 350 degrees." 
+              onChange={e => setNewDirection(e.target.value)} 
+            />
+            <button
+              onClick={() => addItem(directions, setDirections, newDirection, setNewDirection)}
+            >
+              Add
+            </button>
           </div>
           <div>
             <label>Tags:</label>
-            <input type="text" name="tags" placeholder="Comma-separate your list of tags, the more the better!" onChange={e => setTags(e.target.value)} />
+            <p>Tags help users find your recipe. The more, the better!</p>
+            <ul>
+              {makeList(tags, setTags)}
+            </ul>
+            <input 
+              type="text" 
+              placeholder="dinner" 
+              onChange={e => setNewTag(e.target.value)} 
+            />
+            <button
+              onClick={() => addItem(tags, setTags, newTag, setNewTag)}
+            >
+              Add
+            </button>
           </div>
-          <button className="form-button" type="submit">Create Recipe!</button>
+          <button className="form-button" onClick={e => handleSubmit(e)}>Create Recipe!</button>
         </form>
         {redirect}
       </div>
